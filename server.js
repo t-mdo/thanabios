e50f2f0fbf4a6e2d889733002a677f04eeb30ce3
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2018-02-12 02:59:13 +0000

    Add favicon

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const port = process.env.PORT || 8888;
 const app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
