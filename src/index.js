ec185de241401872ff83b8aa4098b86d5ffd1639
Thibault Miranda de Oliveira <thibault.mirandadeoliveira@gmail.com>
2018-02-21 02:36:47 +0000

    Remove test index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import moment from 'moment';
 
 import getStore from './store';
 
 import App from './containers/App';
 
-const initialStateDev = {
-  user: {
-    birthdate: moment('1985-11-05T00:00:00.000Z'),
-    gender: 'male',
-    country: 'France',
-  },
-  life: {
-    settingUp: false,
-    requesting: false,
-    failed: false,
-    failedError: {},
-    lifeExpectancy: 4459.857142857143,
-    weeksPassed: 1684,
-    weeksAhead: 2775,
-  },
-};
-
-const store = getStore(initialStateDev);
+const store = getStore();
 
 ReactDOM.render(
   <Provider store={store}>
